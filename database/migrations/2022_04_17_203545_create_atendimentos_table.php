9ad95c89b5b348e512649cde30c9dd9e9f047fbe
pbenderson <souza98120@gmail.com>
2022-05-12 10:16:52 -0300

    Novo banco criado :+1:, Correção das tabelas
    Tabela criada no banco, já esta em funcionamento
    Falta testar/funcionamento do Carbon :+1:

--- database/migrations/2022_04_17_203545_create_atendimentos_table.php
+++ database/migrations/2022_04_17_203545_create_atendimentos_table.php
@@ -10,24 +10,22 @@ class CreateAtendimentosTable extends Migration
 {
     public function up()
     {
-        Schema::create('tb_atendimentos', function (Blueprint $table) {
+        Schema::create('tb_atendentes', function (Blueprint $table) {
             $table->increments('id_atendimentos');
-            $table->string('cpf');
-            $table->string('numero_atendimento');
-            $table->enum('sufixo_atendimento', ['PDG', 'FCR', 'DRT', 'OTS']) -> default('PDG');
+            $table->int('cpf');
+            $table->int('numero_atendimento');
+            $table->enum('sufixo_atendimento', ['PDG', 'FCR', 'DRT', 'OTS'])->default('PDG');
             $table->string('observacoes')->nullable();
-            $table->time('data_atendimento');
-            $table->time('emissao_atendimento');
-            $table->time('inicio_atendimento')->nullable();
-            $table->time('fim_atendimento')->nullable();
+            $table->date('data_atendimento');
+            $table->timestamp('emissao_atendimento');
+            $table->dateTime('inicio_atendimento')->nullable();
+            $table->dateTime('fim_atendimento')->nullable();
             $table->enum('estado_fim_atendimento', ['nao_concluido', 'concluido'])->nullable();
-
         });
-
     }
 
     public function down()
     {
-        Schema::dropIfExists('tb_atendimentos');
+        Schema::dropIfExists('tb_atendentes');
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,40 +18,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        DB::table('tb_atendimentos')->insert([
-            'cpf' => '12345678901',
+        DB::table('tb_atendentes')->insert([
+            'cpf' => '12345678',
             'numero_atendimento' => '1',
             'sufixo_atendimento' => 'PDG',
-            'observacoes' => 'ontem',
-            'emissao_atendimento' => '10:11:12',
-            'data_atendimento' => '13:14:15',
-            'inicio_atendimento' => '16:17:18',
-            'fim_atendimento' => '19:20:21',
-            //'estado_atendimento'=>'nao_concluido',
-        ]);
-
-        DB::table('tb_atendimentos')->insert([
-            'cpf' => '123456789',
-            'numero_atendimento' => '2',
-            'sufixo_atendimento' => 'FCR',
-            'observacoes' => 'hoje',
-            'emissao_atendimento' => '22:23:24',
-            'data_atendimento' => '25:26:27',
-            'inicio_atendimento' => '28:29:30',
-            'fim_atendimento' => '31:32:33',
-            //'estado_atendimento'=>'nao_concluido',
-        ]);
-
-        DB::table('tb_atendimentos')->insert([
-            'cpf' => '12345678901',
-            'numero_atendimento' => '3',
-            'sufixo_atendimento' => 'OTS',
-            'observacoes' => 'amanha',
-            'emissao_atendimento' => '34:35:36',
-            'data_atendimento' => '37:38:39',
-            'inicio_atendimento' => '40:41:42',
-            'fim_atendimento' => '43:44:45',
-            //'estado_atendimento'=>'nao_concluido',
+            'observacoes' => Str::random(10),
+            'data_atendimento' => '2020-04-17',
+            //'emissao_atendimento' => '2020-04-17 20:35:45',
+            //'inicio_atendimento' => '2020-04-17 20:35:45',
+            //'fim_atendimento' => '2020-04-17 26:36:46',
+            'estado_atendimento' => 'nao_concluido',
         ]);
     }
 }
