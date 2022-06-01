4cf778a687c4dbbdcc7dcbd367855bf5e60e8675
Lucas Pereira Jorge <lucas.p.jorge13@gmail.com>
2022-06-01 16:42:32 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -8,27 +8,36 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//
 Route::get('/atendimentos', [AtendimentoController::class, 'index']);
 
+//
 Route::get('/atendimento/id/{id_atendimento}', [AtendimentoController::class, 'get']);
 
+//
 Route::get('/atendimentos/dia/{date}', [AtendimentoController::class, 'atendimentosDate']);
 
+//
 Route::get('/atendimentos/dias/{from}&{to}', [AtendimentoController::class, 'atendimentosFromTo']);
 
+//
 Route::get('/atendimentos/month/{month}', [AtendimentoController::class, 'atendimentosMonth']);
 
+//
 Route::get('/atendimentos/queue/', [AtendimentoController::class, 'atendimentosQueueToday']);
 
+//
 Route::get('/atendimentos/queue/next', [AtendimentoController::class, 'atendimentosQueueTodayNext']);
 
+//
 Route::get('/atendimentos/afterQueue/', [AtendimentoController::class, 'atendimentosAfterQueueToday']);
 
-
+//
 Route::post('/atendimento/post', [AtendimentoController::class, 'createAtendimento']);
 
 
 //update -> iniciar atendimento
+
 Route::put('/atendimento/begin/{id_atendimento}', [AtendimentoController::class, 'atendimentoBegin']);
 
 //update -> finalizar atendimento
