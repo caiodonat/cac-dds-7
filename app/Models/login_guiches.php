bcbc0df089680b5b4ace4eee276b3cee985c8151
endersonpb <souza98120@gmail.com>
2022-06-07 14:20:37 -0300

    Create migrate loginGuiches/guiches

---
+++ app/Models/login_guiches.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class login_guiches extends Model
+{
+    use HasFactory;
+    protected $table = 'login_guiches';
+    protected $fillable = ['login', 'senha', 'nomes_funcionarios', 'cadastros_ativos'];
+    protected $primaryKey = ['login', 'senha', 'nomes_funcionarios', 'cadastros_ativos'];
+}
