fe4f4ad38b4d1464bc8e6722cd1f1d8927f45dc8
Theendringer <gustepereira@gmail.com>
2022-07-11 14:50:45 -0300

    shell

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "instalando ambiente de desenvolvimento";
+sudo apt install  php php-curl php-xml php-mbstring php-mysql composer vim git curl
+
+echo "instalando nodejs";
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh &&
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash &&
+source ~/.bashrc &&
+nvm install v16.15.1 &&
+npm install -g npm@8.13.2 &&
+
+echo "download vsocde e chrome";
+
+
+echo "clonado projeto tcc";
+cd Documents/teste &&
+git clone https://github.com/CaioDonat/CentralAtendimentoCliente2/tree/servidor &&
+cd CentralAtendimentoCliente2 &&
+git checkout servidor &&
+
+echo "atualizando composer";
+composer update &&
+composer upgrade &&
+composer install &&
+npm install &&
+npm run dev &&
+
+cd ~/ &&
+
+echo "atulizando sistema"
+sudo apt-get update -y &&
+sudo apt-get full-upgrade &&
