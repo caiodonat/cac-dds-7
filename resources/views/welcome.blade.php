089ceb44a6da50324b38b9ac2d289a7c41ce80ed
Theendringer <gustepereira@gmail.com>
2022-07-13 17:12:45 -0300

    merge

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         <div class="container2">
-                            
+
                         </div>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
