03490280f854dee051d064eb3d6c34611f5931ba
LucasPereiraJorge <lucas.p.jorge13@gmail.com>
2022-07-22 14:51:55 -0300

    Testes Api serviços concluida

--- tests/Feature/Api/ServicosTest.php
+++ tests/Feature/Api/ServicosTest.php
@@ -31,4 +31,58 @@ class ServicosTest extends TestCase
             )
         );
     }
+
+    public function test_fcr()
+    {
+        $r = $this->getJson('api/servicos/fcr');
+
+        $r->assertJson(fn (AssertableJson $json) =>
+        $json->has('r')
+        ->where('success', true)
+        ->first(fn ($json1) =>
+            $json1->each(fn($json3) =>
+                $json3->has('id_servicos')
+                    ->has('servico')
+                    ->where('setor', 'FCR')
+                    ->etc()
+                )
+            )
+        );
+    }
+
+    public function test_sct()
+    {
+        $r = $this->getJson('api/servicos/sct');
+
+        $r->assertJson(fn (AssertableJson $json) =>
+        $json->has('r')
+        ->where('success', true)
+        ->first(fn ($json1) =>
+            $json1->each(fn($json4) =>
+                $json4->has('id_servicos')
+                    ->has('servico')
+                    ->where('setor', 'SCT')
+                    ->etc()
+                )
+            )
+        );
+    }
+
+    public function test_ots()
+    {
+        $r = $this->getJson('api/servicos/ots');
+
+        $r->assertJson(fn (AssertableJson $json) =>
+        $json->has('r')
+        ->where('success', true)
+        ->first(fn ($json1) =>
+            $json1->each(fn($json5) =>
+                $json5->has('id_servicos')
+                    ->has('servico')
+                    ->where('setor', 'OTS')
+                    ->etc()
+                )
+            )
+        );
+    }
 }
