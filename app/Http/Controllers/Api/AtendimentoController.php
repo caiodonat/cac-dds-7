07e353ce93a5b472f6b2c42df23b66358f6a06a2
caiodonatt@gmail.com <caiodonatt@gmail.com>
2022-07-24 18:43:51 -0300

    fix post

--- app/Http/Controllers/Api/AtendimentoController.php
+++ app/Http/Controllers/Api/AtendimentoController.php
@@ -2,14 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Requests\StoreAtendimentoRequest;
-use App\Http\Requests\UpdateAtendimentoRequest;
 use App\Http\Controllers\Controller;
-
 use App\Models\Atendimento as Atendimento;
-use App\Models\LoginGuiche;
-use App\Models\Guiche;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\DB;
@@ -27,7 +21,7 @@ class AtendimentoController extends Controller
       $cNow = Carbon::now('-03:00');
       $newA = new Atendimento();
 
-      //$newA->id_atendimento = $rt->input('id_atendimento');
+      //$newA->id_atendimento = $rt->input('id_atendimento');//not possible
       
       if($newA->date_time_emissao_atendimento = $rt->input('date_time_emissao_atendimento')){
         $newA->date_emissao_atendimento = Carbon::create($rt->input('date_time_emissao_atendimento'))->toDateString();
@@ -38,24 +32,31 @@ class AtendimentoController extends Controller
 
       $newA->cpf = $rt->input('cpf');
 
-      if(!$newA->sufixo_atendimento = $rt->input('sufixo_atendimento')){
-        $newA->sufixo_atendimento = "OTS";
-      }
-
       if(!$newA->numero_atendimento = $rt->input('numero_atendimento')){
-        
+        try{
         $lA = DB::table('tb_atendimentos')
         ->where("date_emissao_atendimento", $cNow->toDateString())
         ->get()->last();
 
         $newA->numero_atendimento = $lA->numero_atendimento + 1;
+        } catch (\Throwable){
+          $newA->numero_atendimento = 1;
+        }
+      }
+
+      if($newA->servicos = $rt->input('servicos')){
+        $newA->sufixo_atendimento = DB::table('tb_servicos')
+          ->where('id_servicos', $rt->input('servicos'))
+          ->value('setor');
+      }else{
+        $newA->sufixo_atendimento = "OTS";
       }
       
       $newA->save();
 
       $r = DB::table('tb_atendimentos')
-            ->where('id_atendimento', $newA->id_atendimento)
-            ->get();
+        ->where('id_atendimento', $newA->id_atendimento)
+        ->get();
 
       return json_encode(['r' => $r, 'success' => true], JSON_PRETTY_PRINT);
     } catch (\Throwable $th) {
@@ -77,7 +78,6 @@ class AtendimentoController extends Controller
     }
   }
 
-  //retorna um atendimento expecificado por ID
   public function id($id_atendimento)
   {
     try {
@@ -220,8 +220,23 @@ class AtendimentoController extends Controller
 
   //UPDATE
 
-  public function atendimentoBegin($id_atendimento)
-  { //, $guiche
+  public function begin(Request $rt)
+  {
+    try {
+      $cNow = Carbon::now('-03:00');
+
+      DB::table('tb_atendimentos')
+        ->where('id_atendimento', $rt->input('id_atendimento'))
+        ->update(['started' => $cNow->toDateTimeString(),
+          'id_service_desk' => $rt->input('id_service_desk')]);
+        
+        $r = DB::table('tb_atendimentos')
+          ->where('id_atendimento', $rt->input('id_atendimento'));
+          
+      return json_encode(['r' => $r, 'success' => true], JSON_PRETTY_PRINT);
+    } catch (\Throwable $th) {
+      return json_encode(['r' => $th, 'success' => false], JSON_PRETTY_PRINT);
+    }
     $carbonNow = Carbon::now('-03:00');
     Atendimento::where("id_atendimento", "=", $id_atendimento)
       ->update(['started' => $carbonNow->toDateTimeString()]);
