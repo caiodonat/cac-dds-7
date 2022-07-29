cefe11568f33f4c63bda259e3209a9c35f589586
pbenderson <65699677+EndersonpereiraBarcelos@users.noreply.github.com>
2022-07-29 15:51:11 -0300

    segurança01

--- routes/web.php
+++ routes/web.php
@@ -29,64 +29,64 @@ Route::get('testando', function () {
 }) ->name('testando.aplicativo');
 
 Route::get('telao/telao', function () {
-    return view('telao.telao');
+    return view('telao.telao')->middleware('auth');
 }) ->name('telao');
 
 Route::get('totem/totem', function () {
     return view('totem.totem');
-}) ->name('totem');
+}) ->name('totem')->middleware('auth');
 
 Route::get('/mesa_atendimento/principal', function(){
     return view('mesa_atendimento.principal');
- })->name('principal');
+ })->name('principal')->middleware('auth');
 
  Route::get('/mesa_atendimento/atendimento', function(){
     return view('mesa_atendimento.atendimento');
- })->name('atendimento');
+ })->name('atendimento')->middleware('auth');
 
  Route::get('/mesa_atendimento/chamadoatendimento', function(){
     return view('mesa_atendimento.chamadoatendimento');
- })->name('chamadoatendimento');
+ })->name('chamadoatendimento')->middleware('auth');
 
  Route::get('/mesa_atendimento/inicioatendimento', function(){
     return view('mesa_atendimento.inicioatendimento');
- })->name('inicioatendimento');
+ })->name('inicioatendimento')->middleware('auth');
 
  Route::get('/mesa_atendimento/monitor', function(){
     return view('mesa_atendimento.monitor');
- })->name('monitor');
+ })->name('monitor')->middleware('auth');
 
  Route::get('/mesa_atendimento/triagem', function(){
     return view('mesa_atendimento.triagem');
- })->name('triagem');
+ })->name('triagem')->middleware('auth');
 
  Route::get('/mesa_atendimento/configuracoes', function(){
     return view('mesa_atendimento.configuracoes');
- })->name('configuracoes');
+ })->name('configuracoes')->middleware('auth');
 
  Route::get('/totem/financeiro', function(){
    return view('totem.financeiro');
-})->name('financeiro');
+})->name('financeiro')->middleware('auth');
 
 Route::get('/totem/totem', function(){
    return view('totem.totem');
-})->name('inicio');
+})->name('inicio')->middleware('auth');
 
 Route::get('/totem/outros', function(){
    return view('totem.outros');
-})->name('outros');
+})->name('outros')->middleware('auth');
 
 Route::get('/totem/secretaria', function(){
    return view('totem.secretaria');
-})->name('secretaria');
+})->name('secretaria')->middleware('auth');
 
 Route::get('/totem/pedagogico', function(){
    return view('totem.pedagogico');
-})->name('pedagogico');
+})->name('pedagogico')->middleware('auth');
 
 Route::get('/totem/painel', function(){
    return view('totem.painel');
-})->name('painel');
+})->name('painel')->middleware('auth');
 
 Route::get('/totem/alert', function(){
    return view('totem.alert');
