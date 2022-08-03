f9cf882272df743ae3d36e8aa6781192b2ac3b75
caiodonatt@gmail.com <caiodonatt@gmail.com>
2022-08-03 14:02:48 -0300

    identacao

--- resources/views/layouts/telao.blade.php
+++ resources/views/layouts/telao.blade.php
@@ -1,32 +1,35 @@
 <!doctype html>
 <html lang="pt-BR">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+  <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/script.js') }}" defer></script>
-    <script src="{{ asset('js/mesa.js') }}" defer></script>
+  <!-- Scripts -->
+  <script src="{{ asset('js/script.js') }}" defer></script>
+  <script src="{{ asset('js/mesa.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+  <!-- Fonts -->
+  <link rel="dns-prefetch" href="//fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/telao.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/mesa.css') }}" rel="stylesheet">
+  <!-- Styles -->
+  <link href="{{ asset('css/telao.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/mesa.css') }}" rel="stylesheet">
 </head>
+
 <body onload="teste(); time(); data()">
-    <div id="app">
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-    @stack('myjs')
+  <div id="app">
+    <main class="py-4">
+      @yield('content')
+    </main>
+  </div>
+  @stack('myjs')
 </body>
+
 </html>
