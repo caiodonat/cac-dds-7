97f682da33fcc6d03ab85295b6ad781cca5d083e
caiodonatt@gmail.com <caiodonatt@gmail.com>
2022-08-03 17:01:36 -0300

    queueAlready_called -> reverse query

--- app/Http/Controllers/Api/AtendimentoController.php
+++ app/Http/Controllers/Api/AtendimentoController.php
@@ -177,7 +177,8 @@ class AtendimentoController extends Controller
       $r = DB::table('tb_atendimentos')
         ->where("date_emissao_atendimento", $cNow)
         ->whereNotNull("first_call")
-        ->get();
+        ->get()
+        ->reverse();
 
       return json_encode(['r' => $r, 'success' => true], JSON_PRETTY_PRINT);
     } catch (\Throwable $th) {
