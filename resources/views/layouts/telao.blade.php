c462faa81bbecb9422b970a1af95f1759b8a56c9
caiodonatt@gmail.com <caiodonatt@gmail.com>
2022-08-05 04:08:00 -0300

    fix CORS try-7

--- resources/views/layouts/telao.blade.php
+++ resources/views/layouts/telao.blade.php
@@ -30,7 +30,7 @@
         </main>
     </div>
     @stack('myjs')
-    <h6 id="endPoint" value={{ env('APP_URL') }}>.</h6>
+    <h6 id="endPoint" value={{ env('APP_URL') }}></h6>
 </body>
 
 <script type="text/javascript">
